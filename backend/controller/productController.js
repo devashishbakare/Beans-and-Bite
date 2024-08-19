8238c0b5581f73030b2cff8eab8207c4ec2fd44e
devashish bakare <devashishbakare@gmail.com>
2024-08-19 16:10:50 +0530

    added search and homepage API

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -44,7 +44,65 @@ const addProducts = async (req, res) => {
   }
 };
 
+const bestSellingProducts = async (req, res) => {
+  try {
+    const condition = {
+      category: {
+        $in: ["Bestseller", "Drinks"],
+      },
+    };
+    const response = await Product.find(condition);
+    return res
+      .status(200)
+      .json({ data: response, message: "Best selling products" });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+      message: "Something went wrong, try again later",
+    });
+  }
+};
+const fetchSearchResult = async (req, res) => {
+  try {
+    const searchKey = req.query.key;
+    if (!searchKey) {
+      return res.status(400).json({ message: "search key not found" });
+    }
+
+    const searchWords = searchKey.split(" ");
+    const regexPatterns = searchWords.map((word) => new RegExp(word, "i"));
+    const searchFor = {
+      $or: [
+        { name: regexPatterns },
+        { category: regexPatterns },
+        { productType: regexPatterns },
+      ],
+    };
+
+    const result = await Product.find(searchFor);
+    const searchResult = result.map((product) => {
+      return {
+        id: product._id,
+        image: product.productCartImage,
+        productName: product.name,
+      };
+    });
+
+    return res.status(200).json({
+      data: searchResult,
+      message: "Search Result",
+      regex: searchWords,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+      message: "Something went wrong, try again later",
+    });
+  }
+};
 module.exports = {
   t,
   addProducts,
+  bestSellingProducts,
+  fetchSearchResult,
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const productController = require("../controller/productController");
 
 router.post("/addProduct", productController.addProducts);
+router.get("/bestSelling", productController.bestSellingProducts);
+router.get("/search", productController.fetchSearchResult);
 
 module.exports = router;
